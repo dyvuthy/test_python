406d409227288bcb834f623e60db1f662e4b026d
dyvuthy <napoliking999@gmail.com>
2024-07-20 10:27:59 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+print("hello worlld")
+print("112")
+
+print("single 'quote' inside double quote")
+
+print("single \'quote' and \"double\" quote inside quote")
+
+name=input("what is your name? ")
+print("welcome " + name)
+
+Qty1=input("input qty1= ")
+print("amount " + Qty1)
+Qty2=input("input qty2")
+print("amount " + Qty2)
+
+qty1=int(input("Qty1="))
+qty2=int(input("Qty2="))
+price=float(input("Price= "))
+total_qty=qty1+qty2
+print("Total Qty= " + str(total_qty))
+amount=total_
+
